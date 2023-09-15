026637c2591439ec3f23805fac428c2704633d5d
saumitrasaxena <saxena.saumitra@gmail.com>
2023-09-15 15:40:23 +0530

    End of Section-2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDBContext: DbContext
+    {
+        public NZWalksDBContext(DbContextOptions<NZWalksDBContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Region> Walks { get; set; }
+        public DbSet<Region> WalkDifficulty { get; set; }
+
+    }
+}
